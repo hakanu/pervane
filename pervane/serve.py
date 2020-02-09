bc5115fa997de9d5ec7c3264c135b652d70ababa
hakan <hi@hakanu.net>
2020-02-09 00:20:13 +0000

    Make multi accounts isolated by username meanwhile single user works just fine with passed directory; abstract the working directory from the interface; fixes #59

--- pervane/serve.py
+++ pervane/serve.py
@@ -50,12 +50,22 @@ from jinja2 import Environment, BaseLoader
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import login_required, UserManager, UserMixin
+from flask_user import current_user, login_required, UserManager, UserMixin
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
 mimetypes.init()
+
+def str2bool(v):
+  if isinstance(v, bool):
+   return v
+  if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    return True
+  elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    return False
+  else:
+    raise argparse.ArgumentTypeError('Boolean value expected.')
 
 parser = argparse.ArgumentParser(description='Process input from user.')
 parser.add_argument('--host', dest='host', default='0.0.0.0',
@@ -85,9 +95,13 @@ parser.add_argument(
     dest='front_page_message',
     default='Welcome to Pervane! This is default welcome message.',
     help='Cache the sidebar file tree creation.')
-parser.add_argument('--allow_multi_user', dest='allow_multi_user', default=False,
-                    help='Should pervane allow multiple users to see the same notes? '
-                         'Be careful.')
+parser.add_argument(
+    '--allow_multi_user', type=str2bool, dest='allow_multi_user', default=False,
+    help='Should pervane allow multiple users to see the same notes? Be careful.')
+parser.add_argument(
+    '--debug', type=str2bool, dest='debug', default=False,
+    help='Show debug logs')
+
 args = parser.parse_args()
 # Log may not show up in non-debug mode.
 print('loaded args %s', args)
@@ -96,7 +110,7 @@ if args.username or args.password:
 		'cookie based login, please use that method.')
 
 # We should always work with absolute path in order not to cause security issues
-_ROOT_DIR = os.path.abspath(args.root_dir)
+_WORKING_DIR = os.path.abspath(args.root_dir)
 # Platform independent way of obtaining home folder.
 _PERVANE_CONFIG_DIR = os.path.join(str(pathlib.Path.home()), '.pervane')
 logging.info('config dir: ', _PERVANE_CONFIG_DIR)
@@ -122,14 +136,12 @@ class ConfigClass(object):
   USER_CORPORATION_NAME = 'Pervane'
   USER_COPYRIGHT_YEAR = '2020'
   USER_APP_VERSION = 'alpha'
-  UPLOAD_FOLDER = _ROOT_DIR  
 
 logging.basicConfig(level=logging.DEBUG)
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 app = Flask(__name__, template_folder='templates')
 cache.init_app(app)
 
-UPLOAD_FOLDER = _ROOT_DIR  #'/path/to/the/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app.config.from_object(__name__+'.ConfigClass')
@@ -169,6 +181,10 @@ class CustomUserManager(UserManager):
 
 # Setup Flask-User and specify the User data-model
 user_manager = CustomUserManager(app, db, User)
+
+
+def _get_root_dir():
+  return (_WORKING_DIR if not args.allow_multi_user else os.path.join(_WORKING_DIR, current_user.username)) + os.path.sep
 
 
 def program_installed(binary):
@@ -263,13 +279,20 @@ def _get_file_paths_flat(path):
 @app.route('/')
 @login_required
 def front_page_handler():
+  root_dir = _get_root_dir()
+  if not os.path.exists(root_dir):
+    logging.info('Initializing the root dir')
+    os.mkdir(root_dir)
+    
   return render_template(
-      'index.html', tree=make_tree(_ROOT_DIR),
+      'index.html', tree=make_tree(root_dir),
       html_content=args.front_page_message,
       note_extensions=args.note_extensions,
       mime_type='',
-      file_paths_flat=json.dumps(_get_file_paths_flat(_ROOT_DIR)),
-      root_dir=_ROOT_DIR)
+      file_paths_flat=json.dumps(_get_file_paths_flat(root_dir)),
+      root_dir=root_dir,
+      current_user=current_user,
+      working_dir=_WORKING_DIR)
 
 
 @app.route('/file')
@@ -278,13 +301,16 @@ def file_handler():
   path = request.args.get('f', '')
   if not path:
     return 'No path is given'
-  path = path.strip()
-  if not path.startswith(_ROOT_DIR):
+  # Even though passed path has separators, we get the last piece /hakuna/matata/yo => yo.
+  # Trying to be defensive here against custom requests.
+
+  # Get rid of that separator as first char and join with working dir.
+  # /hakuna/test.md becomes /tmp/notes/hakuna/test.md
+  path = os.path.join(_WORKING_DIR, path[1:])
+  root_dir = _get_root_dir() 
+  if not path.startswith(root_dir):
     logging.info('no auth')
-    return (
-        'Not authorized to see this dir, must be under: ' +
-        _ROOT_DIR)
-  path = path.strip()
+    return ('Not authorized to see this dir')
   html_content = ''
   content = ''
   mime_type = get_mime_type(path)
@@ -314,20 +340,25 @@ def file_handler():
 
   _, ext = os.path.splitext(path)
   return render_template('index.html',
-      path=path, html_content=html_content, md_content=content, ext=ext,
-      tree=make_tree(_ROOT_DIR), mime_type=mime_type,
+      path=path.replace(_WORKING_DIR, ''),
+      html_content=html_content, md_content=content, ext=ext,
+      tree=make_tree(root_dir), mime_type=mime_type,
       note_extensions=args.note_extensions,
-      file_paths_flat=json.dumps(_get_file_paths_flat(_ROOT_DIR)),
-      root_dir=_ROOT_DIR)
+      file_paths_flat=json.dumps(_get_file_paths_flat(root_dir)),
+      root_dir=root_dir,
+      current_user=current_user,
+      working_dir=_WORKING_DIR)
 
 
 @app.route('/api/get_content')
 @login_required
 def api_get_content_handler():
-  path = request.args.get('f', '')
-  if not path.startswith(_ROOT_DIR):
+  path = request.args.get('f', '').strip()
+  path = os.path.join(_WORKING_DIR, path[1:])
+  root_dir = _get_root_dir()
+  if not path.startswith(root_dir):
     logging.info('no auth')
-    return 'Not authorized to see this dir, must be under: %s' % _ROOT_DIR
+    return 'Not authorized to see this dir' 
   try:
     with open(path, 'r') as f:
       content = f.read()
@@ -340,11 +371,12 @@ def api_get_content_handler():
 @login_required
 def api_update_handler():
   updated_content = request.form.get('updated_content', '').strip()
-  file_path = request.form.get('file_path', '').strip()
+  root_dir = _get_root_dir()
+  file_path = os.path.join(_WORKING_DIR, request.form.get('file_path', '').strip()[1:])
   if not file_path:
     return jsonify({'result': 'File path is empty'})
   
-  if not file_path.startswith(_ROOT_DIR):
+  if not file_path.startswith(root_dir):
     return jsonify({'result': 'Unauth file modification'})
 
   if not updated_content:
@@ -360,68 +392,80 @@ def api_update_handler():
 @app.route('/api/add_node', methods=['POST'])
 @login_required
 def add_node_handler():
-    parent_path = request.form.get('parent_path', '').strip()
-    new_node_name = request.form.get('new_node_name', '').strip()
-    if not parent_path or not new_node_name:
+  root_dir = _get_root_dir()
+
+  # Parent path comes like /username or / so the real parent path needs to be built.
+  # Remove / from the GET param.
+  parent_path = os.path.join(_WORKING_DIR, request.form.get('parent_path', '').strip()[1:])
+
+  # Eliminate file separator.
+  new_node_name = request.form.get('new_node_name', '').strip()
+  is_dir = False
+  if new_node_name.endswith(os.path.sep):
+    is_dir = True
+    # Strip the /.
+    new_node_name = new_node_name[:-1]
+  if not parent_path or not new_node_name:
+    return jsonify({
+        'result': 'fail',
+        'message': 'Path can not be empty',
+    })
+  new_node_name = new_node_name.strip()
+
+  if not parent_path.startswith(root_dir):
+    return jsonify({
+        'result': 'fail',
+        'message': 'Unauth file modification',
+    })
+
+  if is_dir:
+    path = os.path.join(parent_path, new_node_name)
+    logging.info('Creating new node as dir %s', path)
+    try:
+      os.mkdir(path)
+    except OSError:
       return jsonify({
           'result': 'fail',
-          'message': 'Path can not be empty',
+          'message': 'Failed create this directory: ' +
+          path +
+          '. Make sure parent directory exists and  you have write '
+          'access to the parent directory.',
+          'entity': path,
       })
-    new_node_name = new_node_name.strip()
-
-    if not parent_path.startswith(_ROOT_DIR):
-      return jsonify({
-          'result': 'fail',
-          'message': 'Unauth file modification',
-      })
-
-    if new_node_name.endswith('/'):
-      path = os.path.join(parent_path, new_node_name)
-      logging.info('Creating new node as dir %s', path)
-      try:
-        os.mkdir(path)
-      except OSError:
-        return jsonify({
-            'result': 'fail',
-            'message': 'Failed create this directory: ' +
-            path +
-            '. Make sure parent directory exists and  you have write '
-            'access to the parent directory.',
-            'entity': path,
-        })
-      else:
-        return jsonify({
-            'result':  'success', 
-            'message': 'created the directory',
-            'entity': path,
-        })
     else:
-      suffix = '' if new_node_name.endswith('.md') else '.md'
-      path = os.path.join(parent_path, new_node_name + suffix)
-      try:
-        f = open(path, 'x')
-      except OSError:
-        return jsonify({
-            'result':  'fail',
-            'message': 'failed to create markdown file: ' + path,
-            'entity': path,
-        })
-      else:
-        return jsonify({
-            'result':  'success',
-            'message': 'created the directory: ' + path,
-            'entity': path,
-        })
+      return jsonify({
+          'result':  'success', 
+          'message': 'created the directory',
+          'entity': os.path.join(path, os.path.sep),
+      })
+  else:
+    suffix = '' if new_node_name.endswith('.md') else '.md'
+    path = os.path.join(parent_path, new_node_name + suffix)
+    try:
+      f = open(path, 'x')
+    except OSError:
+      return jsonify({
+          'result':  'fail',
+          'message': 'failed to create markdown file: ' + path,
+          'entity': path,
+      })
+    else:
+      return jsonify({
+          'result':  'success',
+          'message': 'created the directory: ' + path.replace(_WORKING_DIR, ''),
+          'entity': path.replace(_WORKING_DIR, ''),
+      })
 
 
 @app.route('/api/move_file')
 @login_required
 def api_move_handler():
-  source_path = request.args.get('source_path', '')
-  dest_dir = request.args.get('dest_dir', '')
-  if not source_path.startswith(_ROOT_DIR) or not dest_dir.startswith(_ROOT_DIR):
+  source_path = os.path.join(_WORKING_DIR, request.args.get('source_path', '').strip()[1:])
+  root_dir = _get_root_dir()
+  dest_dir = os.path.join(_WORKING_DIR, request.args.get('dest_dir', '').strip()[1:])
+  if not source_path.startswith(root_dir) or not dest_dir.startswith(root_dir):
     logging.info('no auth')
-    return 'Not authorized to see this dir, must be under: %s' % _ROOT_DIR
+    return 'Not authorized to see this dir, must be under: %s' % root_dir
   try:
     base_name = os.path.basename(source_path)
     dest_path = os.path.join(dest_dir, base_name)
@@ -439,19 +483,18 @@ def api_move_handler():
 @app.route('/search')
 @login_required
 def search_handler():
+  root_dir = _get_root_dir()
   query = request.args.get('query', '')
   if not query:
     return 'You need to search for something'
-  # if not program_installed('ag') and not program_installed('ack'):
-  #   return 'You should install either silver searcher (ag) or ack for fast search'
 
-  # cmd = ''
+  cmd = ''
   if program_installed('ag'):
     # ackmate mode for re-using same parsing logic.
-    cmd = ['ag', query, _ROOT_DIR, '--ackmate', '--stats', '-m', '2']
+    cmd = ['ag', query, root_dir, '--ackmate', '--stats', '-m', '2']
     logging.info('Using ag for search')
   else:
-    cmd = ['ack', query, _ROOT_DIR, '--column', '--heading'] 
+    cmd = ['ack', query, root_dir, '--column', '--heading'] 
     logging.info('Using ack for search')
   # TODO(hakanu): fallback to find.
   logging.info('Running cmd: %s', cmd)
@@ -477,7 +520,7 @@ def search_handler():
   for line in lines[:tail]:
     # Check if the line starts with a number or a letter.
     # Number indicates that it's a file match starting.
-    if line.startswith(prefix + _ROOT_DIR):
+    if line.startswith(prefix + root_dir):
       fn = line
       in_file_results = []
       file_finished = False
@@ -489,7 +532,7 @@ def search_handler():
       })
 
       results.append({
-        'file': fn.replace(':', ''),
+        'file': fn.replace(':', '').replace(_WORKING_DIR, ''),
         'matches': in_file_results,
       }) 
 
@@ -497,14 +540,17 @@ def search_handler():
   return render_template(
       'index.html',
       search_results=results, query=query, stats=stats_str,
-      tree=make_tree(_ROOT_DIR),
+      tree=make_tree(root_dir),
       note_extensions=args.note_extensions, mime_type='',
-      root_dir=_ROOT_DIR)
+      root_dir=root_dir,
+      current_user=current_user,
+      working_dir=_WORKING_DIR)
 
 
 @app.route('/upload', methods=['POST'])
 @login_required
 def file_upload_handler():
+  root_dir = _get_root_dir()
   # check if the post request has the file part
   if 'file' not in request.files:
     flash('No file part')
@@ -519,7 +565,7 @@ def file_upload_handler():
     filename = secure_filename(file.filename)
     base_name, extension = os.path.splitext(filename)
     dest_path = os.path.join(
-        app.config['UPLOAD_FOLDER'], base_name + '_' + 
+        root_dir, base_name + '_' + 
         datetime.datetime.now().strftime('%Y%m%d_%H%M') + extension)
     file.save(dest_path)
     logging.info('Upload is successful, refreshing the current page '
@@ -527,7 +573,7 @@ def file_upload_handler():
     return jsonify({
         'result': 'success',
         'message': 'File is successfully uploaded.',
-        'entity': dest_path,
+        'entity': dest_path.replace(_WORKING_DIR, ''),
     })
   else:
     return jsonify({
@@ -539,9 +585,9 @@ def file_upload_handler():
 
 def cli_main():
     """Used within the python package cli."""
-    app.run(host=args.host, port=args.port)
+    app.run(host=args.host, port=args.port, debug=args.debug)
 
 
 if __name__ == '__main__':
-  app.run(host=args.host, port=args.port)
+  app.run(host=args.host, port=args.port, debug=args.debug)
 
